a621efde7cb52ebcd910f7e9435bf472779aee73
Gabriela Alves <gabrielaalvesdagama@gmail.com>
2019-10-03 13:00:47 -0300

    Adicionando função recursiva em ultrapassa.c

--- LISTA4/ultrapassa.c
+++ LISTA4/ultrapassa.c
@@ -2,36 +2,39 @@
 Universidade de Brasília - UnB Campus Gama (FGA)*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void verifica(int *numeros,int limite){
-  for (int i = 0; i < n; i++){
-    soma = soma + numeros[i];
-    if (soma > 90)
+void verifica(int *numeros,int i){
+  int soma = 0;
+    if (numeros[i]==90)
       printf("%d\n",numeros[i]);
-    verifica(numeros,n,i,soma);
-  }
-  printf("%d\n",soma );
+    else {
+      while (soma < 90){
+        soma = soma + numeros[i];
+        i++;
+      }
+      if (soma > 90) {
+       verifica(numeros,i+1);
+       printf("%d\n",numeros[i]);
+   }
+ }
 }
 
 
 int main () {
 
-// Criar um vetor com alocação dinâmica
-// Realocar memória caso vetor > n-1
-// guarda todos os números lidos num vetor
-// verificar e somando
-  int n = 0;
-  int numero;
   int *numeros;
   int i = 0;
-  *numeros = malloc(10*sizeof(int));
 
-  for (int i = 0; numeros[i]==0;i++){
-    if(n[i])
+  numeros = malloc(10*sizeof(int));
 
-  }
+  while(scanf("%d",&numeros[i]),numeros[i]) {
+     if (i > 10) {
+       numeros = realloc(numeros,2*sizeof(numeros));
+     }
+      i++;
+   }
 
-  verifica(numeros,n,0,0);
-
+  verifica(numeros,0);
   return 0;
 }
