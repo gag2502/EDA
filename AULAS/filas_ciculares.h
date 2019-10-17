eb189808bd11156c416d4801401eaf9f430ae5cc
Gabriela Alves <gabrielaalvesdagama@gmail.com>
2019-10-17 12:41:20 -0300

    Conteúdo da aula de filas circulares

---
+++ AULAS/filas_ciculares.h
@@ -0,0 +1,9 @@
+#ifndef filas_circulares_h
+#define filas_circulares_h
+
+int cria_fila();
+int enfileira(int x);
+int desenfileira(int *x);
+int redimensiona();
+
+#endif
---
+++ AULAS/filas_circulares.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *fila;
+int p,u,N;
+
+int cria_fila() {
+  p = 0;
+  u = 0;
+  N = 100;
+  fila = malloc(N*sizeof(int));
+  if (fila == NULL)
+    return 0;
+  return 1;
+}
+
+int redimensiona() {
+  int i,j;
+  fila = realloc(fila, 2*N*sizeof(int));
+  if (fila == NULL)
+    return 0;
+  if (p > u){
+    if(N - p > u){
+      for(i = 0, j = N; i < u; i++, j++)
+        fila[i] = fila[j];
+      u = N + u;
+    }  else {
+        for (i = N - 1,j = 2*N-1; i >= p; i--, j--)
+          fila[j] = fila[i];
+        p = j +1;
+      }
+    }
+  }
+}
+
+int enfileira(int x) {
+  if ((u+1)%N == p){
+    if(redimensiona()){
+      fila[u] = x;
+      u = (u+1)%N;
+      return 1;
+    }
+    return 1;
+  }
+}
